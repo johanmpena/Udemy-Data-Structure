a81594f601e9b4e1b2ab23962af8efea9b18c638
Johan <johanmpena@aim.com>
2021-06-08 12:40:59 -0400

    Simple recursion.

---
+++ Udemy Data Structure/src/Recursion.java
@@ -0,0 +1,23 @@
+
+public class Recursion {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(recursiveFactorial(3));
+	}
+	
+	// 1! = 1 * 0! = 1
+    // 2! = 2 * 1 = 2 * 1!
+    // 3! = 3 * 2 * 1 = 3 * 2!
+    // 4! = 4 * 3 * 2 * 1 = 4 * 3!
+	// n! = n *(n - 1)!
+	
+	public static int recursiveFactorial(int num) {
+		
+		if(num == 0) {
+			return 1;
+		}
+		
+		return num * recursiveFactorial(num - 1);
+	}
+}
